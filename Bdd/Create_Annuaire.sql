5440cb7028e141710b47c7609bf273ce1ed8d00c
Frantzen Christian <frantzenchristian@hotmail.com>
2016-04-19 10:32:48 +0200

    reformat sql file

--- Bdd/Create_Annuaire.sql
+++ Bdd/Create_Annuaire.sql
@@ -1,89 +1,89 @@
-CREATE TABLE User (
-    UID INT(6) NOT NULL AUTO_INCREMENT,
-    Nickname VARCHAR(20) NOT NULL,
-    Email VARCHAR(20),
-    Password VARCHAR(15),
-    EntryDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Admin BIT,
-    PRIMARY KEY (UID)
+CREATE TABLE users (
+    uid INT(6) NOT NULL AUTO_INCREMENT,
+    nickname VARCHAR(20) NOT NULL,
+    email VARCHAR(20) NOT NULL,
+    password VARCHAR(15) NOT NULL,
+    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    admin BIT NOT NULL,
+    PRIMARY KEY (uid)
 );
 
-CREATE TABLE Establishment (
-    EID INT(6) NOT NULL AUTO_INCREMENT,
-    EName VARCHAR(20) NOT NULL,
-    Street VARCHAR(20),
-    HouseNumber INT(5),
-    Zip INT(4),
-    City VARCHAR(20),
-    Longitude INT(5),
-    Latitude INT(5),
-    Tel VARCHAR(20),
-    Site VARCHAR(20),
-    UID INT(6),
-    EntryDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (EID),
-    FOREIGN KEY (UID) REFERENCES User(UID)
+CREATE TABLE establishments (
+    eid INT(6) NOT NULL AUTO_INCREMENT ,
+    ename VARCHAR(20) NOT NULL,
+    street VARCHAR(20) NOT NULL,
+    house_num INT(5) NOT NULL,
+    zip INT(4) NOT NULL,
+    city VARCHAR(20) NOT NULL,
+    longitude INT(5) NOT NULL,
+    latitude INT(5) NOT NULL,
+    tel VARCHAR(20) NOT NULL,
+    site VARCHAR(20) NOT NULL,
+    uid INT(6) NOT NULL,
+    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (uid) REFERENCES users(uid)
 );
 
-CREATE TABLE Restaurant (
-    EID INT(6) NOT NULL,
-    PriceRange INT(3),
-    BanquetCapacity INT(3),
-    Takeaway BIT,
-    Delivery BIT,
-    PRIMARY KEY (EID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE restaurants (
+    eid INT(6) NOT NULL,
+    price_range INT(3) NOT NULL,
+    banquet_capacity INT(3) NOT NULL,
+    takeaway BIT NOT NULL,
+    delivery BIT NOT NULL,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
 
-CREATE TABLE RestaurantClosingDays (
-    EID INT(6) NOT NULL,
-    ClosingDay ENUM('MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT', 'SUN'),
-    Hour ENUM('AM', 'PM'),
-    PRIMARY KEY (EID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE restaurant_closing_days (
+    eid INT(6) NOT NULL,
+    closing_day ENUM('MON', 'TUE', 'WEN', 'THU', 'FRI', 'SAT', 'SUN') NOT NULL,
+    hour ENUM('AM', 'PM') NOT NULL,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
 
-CREATE TABLE Bar (
-    EID INT(6) NOT NULL,
-    Smoking BIT,
-    Snack BIT,
-    PRIMARY KEY (EID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE bars (
+    eid INT(6) NOT NULL,
+    smoking BIT NOT NULL,
+    snack BIT NOT NULL,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
 
-CREATE TABLE Hotel (
-    EID INT(6) NOT NULL,
-    Stars INT(1),
-    Rooms INT(3),
-    StandartPrice INT(3),
-    PRIMARY KEY (EID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE hotels (
+    eid INT(6) NOT NULL,
+    stars INT(1) NOT NULL,
+    rooms INT(3) NOT NULL,
+    standart_price INT(3) NOT NULL,
+    PRIMARY KEY (eid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
 
-CREATE TABLE Comment (
-    CID INT(6) NOT NULL AUTO_INCREMENT,
-    UID INT(6),
-    EID INT(6),
-    EntryDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    Score INT(1),
-    Text TEXT,
-    PRIMARY KEY (CID),
-    FOREIGN KEY (UID) REFERENCES User(UID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE comments (
+    cid INT(6) NOT NULL AUTO_INCREMENT,
+    uid INT(6) NOT NULL,
+    eid INT(6) NOT NULL,
+    entry_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    score INT(1) NOT NULL,
+    Text TEXT NOT NULL,
+    PRIMARY KEY (cid),
+    FOREIGN KEY (uid) REFERENCES users(uid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
 
-CREATE TABLE Tag (
-    TID INT(6) NOT NULL AUTO_INCREMENT,
-    TName VARCHAR(35) NOT NULL,
-    PRIMARY KEY (TID)
+CREATE TABLE tags (
+    tid INT(6) NOT NULL AUTO_INCREMENT,
+    tname VARCHAR(35) NOT NULL,
+    PRIMARY KEY (tid)
 );
 
-CREATE TABLE EstablishmentTag (
-    TID INT(6),
-    EID INT(6),
-    UID INT(6),
-    PRIMARY KEY (TID),
-    FOREIGN KEY (TID) REFERENCES Tag(TID),
-    FOREIGN KEY (UID) REFERENCES User(UID),
-    FOREIGN KEY (EID) REFERENCES Establishment(EID)
+CREATE TABLE establishment_tags (
+    tid INT(6) NOT NULL,
+    eid INT(6) NOT NULL,
+    uid INT(6) NOT NULL,
+    PRIMARY KEY (tid),
+    FOREIGN KEY (tid) REFERENCES tags(tid),
+    FOREIGN KEY (uid) REFERENCES users(uid),
+    FOREIGN KEY (eid) REFERENCES establishments(eid)
 );
