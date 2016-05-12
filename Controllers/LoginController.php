ae82107fbc656289da76412a9fbac21ba1c14744
Frantzen Christian <frantzenchristian@hotmail.com>
2016-05-12 18:23:00 +0200

    if cookie = active login page redirects tu userprofile page

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -4,39 +4,45 @@
         }
 
         public function run() {
+            require_once('Controllers/CookieController.php');
+            $controller = new CookieController();
             
-            $notification = "";
-            
-            if (empty ( $_POST ) || !isset($_POST['username']) || !isset($_POST['password']) ) {#if no connexion is attempted
-                require_once (VIEWSPATH . 'login.php');
-                $notification = "Not sucessful";
+            if ($controller->checkCookies()) {
+                header("Location:?action=userProfile&user=".$_COOKIE['username']);
                 die();
-            }
-            
-            elseif (Db::getInstance()->validLogin((htmlentities ( $_POST ['username'] )), (htmlentities ( $_POST ['password'] )))) {
-                header('Location: ?action=userProfile&user='.$_POST['username']);
+            }else{
+                $notification = "";
                 
-                if (isset($_POST['rememberme'])) {
-                /* Set cookie to last 1 hour */
-                    setcookie('username', $_POST['username'], time()+60*60);
-                    setcookie('uid', Db::getInstance()->getUIDof($_POST['username']), time()+60*60);
-                } else {
-                /* Cookie expires when browser closes */
-                    setcookie('username', $_POST['username'], false);
-                    setcookie('uid',Db::getInstance()->getUIDof($_POST['username']), false);
+                if (empty ( $_POST ) || !isset($_POST['username']) || !isset($_POST['password']) ) {#if no connexion is attempted
+                    require_once (VIEWSPATH . 'login.php');
+                    $notification = "Not sucessful";
+                    die();
                 }
-                die();
                 
-            } else { // password or username false
-                if( ! Db::getInstance()->checkIfUserExists($_POST ['username']) )
-                    $notification = "Username does not exist";
-                else
-                    $notification = "Password incorrect";
+                elseif (Db::getInstance()->validLogin((htmlentities ( $_POST ['username'] )), (htmlentities ( $_POST ['password'] )))) {
+                    header('Location: ?action=userProfile&user='.$_POST['username']);
                     
-                require_once (VIEWSPATH . 'login.php');
-                die(); 
+                    if (isset($_POST['rememberme'])) {
+                    /* Set cookie to last 1 hour */
+                        setcookie('username', $_POST['username'], time()+60*60);
+                        setcookie('uid', Db::getInstance()->getUIDof($_POST['username']), time()+60*60);
+                    } else {
+                    /* Cookie expires when browser closes */
+                        setcookie('username', $_POST['username'], false);
+                        setcookie('uid',Db::getInstance()->getUIDof($_POST['username']), false);
+                    }
+                    die();
+                    
+                } else { // password or username false
+                    if( ! Db::getInstance()->checkIfUserExists($_POST ['username']) )
+                        $notification = "Username does not exist";
+                    else
+                        $notification = "Password incorrect";
+                        
+                    require_once (VIEWSPATH . 'login.php');
+                    die(); 
+                }
             }
-            
         }
     }
 ?>
